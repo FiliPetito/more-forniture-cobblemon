f906de545365e091bd98ddb3f3f3c1e09b27ab92
Filippo Petito <filippo.petito67@gmail.com>
2025-10-18 23:01:22 +0200

    Fixed translation

--- src/main/java/it/fpetito/moreforniturecobblemon/MoreFornitureCobblemonDataGenerator.java
+++ src/main/java/it/fpetito/moreforniturecobblemon/MoreFornitureCobblemonDataGenerator.java
@@ -1,11 +1,19 @@
 package it.fpetito.moreforniturecobblemon;
 
+import it.fpetito.moreforniturecobblemon.datagen.ModModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class MoreFornitureCobblemonDataGenerator implements DataGeneratorEntrypoint {
+
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		MoreFornitureCobblemon.LOGGER.info("[{}] - Data Generator initializing!", MoreFornitureCobblemon.MOD_ID);
 
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+
+		pack.addProvider(ModModelProvider::new);
+
+		MoreFornitureCobblemon.LOGGER.info("[{}] - Data Generator initialization ended", MoreFornitureCobblemon.MOD_ID);
 	}
 }
